12581893650b51f59f520e7ef4e36677c4758802
icefiresS <me@icefires.cc>
2018-04-16 08:20:32 +0800

    Initial Commit

---
+++ CryptoforFun/CryptoforFun.h
@@ -0,0 +1,19 @@
+//
+//  CryptoforFun.h
+//  CryptoforFun
+//
+//  Created by icefires on 2018/4/16.
+//  Copyright © 2018年 icefires. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CryptoforFun.
+FOUNDATION_EXPORT double CryptoforFunVersionNumber;
+
+//! Project version string for CryptoforFun.
+FOUNDATION_EXPORT const unsigned char CryptoforFunVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptoforFun/PublicHeader.h>
+
+
